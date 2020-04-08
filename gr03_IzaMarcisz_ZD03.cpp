163c7bbd7b3229ce11814a7f29470eb80778508b
Iza Marcisz <53024492+marcisziza@users.noreply.github.com>
2020-04-08 22:17:08 +0200

    Add files via upload
    Added class Circle and another show_info() global function.

--- gr03_IzaMarcisz_ZD03.cpp
+++ gr03_IzaMarcisz_ZD03.cpp
@@ -37,16 +37,16 @@ class Rectangle {
 private:
   friend void show_info(Rectangle rec1);
   friend class Point;
-  Point down_left;
+  Point bottom_left;
   double width, height;
 public:
   Rectangle() {
-    down_left.set_x(-6); down_left.set_y(-8);
+    bottom_left.set_x(-6); bottom_left.set_y(-8);
     width = 13;
     height = 10;
   }
   Point get_apex() {
-    return down_left;
+    return bottom_left;
   }
   double get_width() {
     return width;
@@ -54,6 +54,27 @@ public:
   double get_height() {
     return height;
   }  
+};
+
+class Circle {
+private:
+  Point center;
+  int radius;
+public:
+  Circle() {
+    center.set_x(0); center.set_y(0);
+    radius = 10;
+  }
+  Point get_center() {
+    return center;
+  }
+  int get_radius() {
+    return radius;
+  }
+  bool is_tangent(Circle circle) {
+    return true;
+    return false;
+  }
 };
 
 void setXY(Point &p1, double x, double y) { //funkcja globalna umoliwiajaca zmiane wartosci pol obiektu klasy Point
@@ -65,15 +86,25 @@ void setXY(Point &p1, double x, double y) { //funkcja globalna umoliwiajaca zmia
 
 void show_info(Rectangle rec1) {
   Point apex = rec1.get_apex();
-  cout << "down left: (" << apex.get_x() << ", " << apex.get_y() << ")" << endl;
-  cout << "down right: (" << (apex.get_x() + rec1.get_width() ) << ", " << apex.get_y() << ")" << endl;
-  cout << "top right: (" << (apex.get_x() + rec1.get_width() ) << ", " << (apex.get_y() + rec1.get_height() ) << ")" << endl;
-  cout << "top left: (" << apex.get_x() << ", " << (apex.get_y() + rec1.get_height() ) << ")" << endl;
+
+  cout << "Bottom left: (" << apex.get_x() << ", " << apex.get_y() << ")" << endl;
+  cout << "Bottom right: (" << (apex.get_x() + rec1.get_width() ) << ", " << apex.get_y() << ")" << endl;
+  cout << "Top right: (" << (apex.get_x() + rec1.get_width() ) << ", " << (apex.get_y() + rec1.get_height() ) << ")" << endl;
+  cout << "Top left: (" << apex.get_x() << ", " << (apex.get_y() + rec1.get_height() ) << ")" << endl;
+}
+
+void show_info(Circle circle) {
+  Point center = circle.get_center();
+
+  cout << "center: (" << center.get_x() << ", " << center.get_y() << ")" << endl;
+  cout << "radius: " << circle.get_radius();
 }
  
 int main() {
   Point p1, p2, p3;
+
   // cout << "p1: "; p1.print(); //utworzenie testowego punktu konstruktorem domyslnym i wypisanie go na ekranie
+
   setXY(p1, 5, 2); cout << "p1 after using 'setXY' function: "; p1.print();
   setXY(p2, -7, -7); cout << "p2 after using 'setXY' function: "; p2.print();
   setXY(p3, 1.25, 2.5); cout << "p3 after using 'setXY' function: "; p3.print(); 
@@ -85,7 +116,11 @@ int main() {
   cout << "if p2 is placed in rec1 print '1' if not print '0': " << p2.is_common(rec1) << endl;
   cout << "if p3 is placed in rec1 print '1' if not print '0': " << p3.is_common(rec1) << endl << endl;
 
-  show_info(rec1);
+  show_info(rec1); cout << endl;
+
+  Circle circle;
+
+  show_info(circle);
 }
 
 bool Point::is_common(Rectangle rec1) {
